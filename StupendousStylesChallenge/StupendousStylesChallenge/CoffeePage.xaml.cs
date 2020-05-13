1cdfa84d5856482051361b640fa701e34a015ca8
DESKTOP-INVG9UQ\oroma <oromar.melo@gmail.com>
2020-05-13 17:45:49 -0300

    Stupendous Styles Challenge

---
+++ StupendousStylesChallenge/StupendousStylesChallenge/CoffeePage.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace StupendousStylesChallenge
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class CoffeePage : Page
+    {
+        public CoffeePage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void HandleCoffe_Click(object sender, RoutedEventArgs e)
+        {
+            var option = sender as MenuFlyoutItem;
+            var result = string.Empty;
+           if (option.Text != "None")
+            {
+                if (string.IsNullOrEmpty(CoffeeResultText.Text))
+                {
+                    result = option.Text;
+                }
+                else
+                {
+                    result = CoffeeResultText.Text + " + " + option.Text;
+                }
+                CoffeeResultText.Text = result;
+            }
+        }
+    }
+}
---
+++ StupendousStylesChallenge/StupendousStylesChallenge/MainPage.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace StupendousStylesChallenge
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            NavigationFrame.Navigate(typeof(DonutPage));
+        }
+
+        private void NavigationButton_Click(object sender, RoutedEventArgs e)
+        {
+            var to = sender as Button;
+            switch (to.Name)
+            {
+                case nameof(DonutButton):
+                    NavigationFrame.Navigate(typeof(DonutPage));
+                    break;
+                case nameof(CoffeeButton):
+                    NavigationFrame.Navigate(typeof(CoffeePage));
+                    break;
+                case nameof(ScheduleButton):
+                    NavigationFrame.Navigate(typeof(SchedulePage));
+                    break;
+                case nameof(CompleteButton):
+                    NavigationFrame.Navigate(typeof(CompletePage));
+                    break;
+            }
+        }
+    }
+}
