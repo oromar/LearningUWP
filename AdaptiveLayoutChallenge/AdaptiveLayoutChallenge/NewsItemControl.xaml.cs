6acf0ae4e48d78d6db9d635919c93b2d665e3b80
DESKTOP-INVG9UQ\oroma <oromar.melo@gmail.com>
2020-05-25 17:34:38 -0300

    Fixes

---
+++ AdaptiveLayoutChallenge/AdaptiveLayoutChallenge/NewsItemControl.xaml.cs
@@ -0,0 +1,18 @@
+﻿using AdaptiveLayoutChallenge.Models;
+using Windows.UI.Xaml.Controls;
+
+// The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace AdaptiveLayoutChallenge
+{
+    public sealed partial class NewsItemControl : UserControl
+    {
+
+        public NewsItem NewsItem { get { return DataContext as NewsItem; } }
+        public NewsItemControl()
+        {
+            this.InitializeComponent();
+            DataContextChanged += (s, e) => Bindings.Update();
+        }
+    }
+}
